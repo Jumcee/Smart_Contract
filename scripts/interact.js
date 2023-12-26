999b3a9e40dc39e032fa0e5d757e93fbe2313561
Jumcee <futhmah456@gmail.com>
2023-12-26 17:44:35 +0100

    onboarding-user

---
+++ scripts/interact.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const provider = new ethers.AlchemyProvider('sepolia', API_KEY)
+
+const contract = require("../artifacts/contracts/UserOnboarding.sol/UserOnboarding.json");
+
+const privateKey = process.env.PRIVATE_KEY
+const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+const abi = contract.abi
+const contractAddress = process.env.CONTRACT_ADDRESS
+// Create a contract instance
+const userOnboarding = new ethers.Contract(contractAddress, abi, signer)
+
+// Interact with the contract
+async function onboardUser() {
+    const tx = await userOnboarding.onboardUser("0xEF416430BA75942b76bC195deE0aAD8b43B53eac", "USERNAME", 123456789);
+    await tx.wait();
+    console.log("User onboarded successfully!");
+}
+
+// Call the function
+onboardUser()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
